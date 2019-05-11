8249420b695af6a3e75a71f4fd5526b991b1ee09
Royce <royce236@gmail.com>
2019-05-11 11:39:27 +0800

    Classes Modification

--- classes.1.py
+++ classes.1.py
@@ -6,11 +6,19 @@
 
 class User():
     # methods with double underscores are called Dunder Methods like __init__()
+    age = 20 # this is an example of a global variable
+    
     def __init__(self, full_name, birthday):
         # The first method self is used to reference the instance example: user1
+        # we attach self. to a variable name it is attached to that instance
+        # not attaching instance means that it is a global variable and every instance can modify it
         self.name = full_name
         self.birthday = birthday
  
 user1 = User("Royce Chua","10231997")
 print(user1.name)
 print(user1.birthday)
+user1.age = 21
+print(user1.age)
+user2 = User("Chris Mango", "19931205")
+print(user2.age)
